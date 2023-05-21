376248adde8f85a25c248ebc4d3c5043943a487f
codingkid <jjg1014@naver.com>
2023-05-21 19:06:02 +0900

    first commit java spring setting and git push_test

---
+++ main/java/kr/or/jjg/TestMainController.java
@@ -0,0 +1,14 @@
+package kr.or.jjg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestMainController {
+
+	@RequestMapping(value = {"/" , "/index.do"}, method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+}
---
+++ main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>git & Spring setting 연습해보기</h1>
+	<h3> git init</h3>
+	<h3> git add</h3>
+	<h3> git commit</h3>
+	<h3> git push</h3>
+</body>
+</html>
